de9f8e70d73c1e36f722855524f8aeea2c002658
deybyr647 <deybyr647@gmail.com>
2021-02-09 01:10:17 -0500

    Added content to homepage. Will proceed to integrate map page, responsive design & color scheme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,42 @@
 import Head from 'next/head';
-import {Container, Row, Col, Jumbotron, Image, Carousel} from 'react-bootstrap';
-import {BiRecycle} from 'react-icons/bi';
+import { Container, Row, Col, Jumbotron, Image, Carousel, Accordion, Card, Button, AccordionContext, useAccordionToggle } from 'react-bootstrap';
+import { BiRecycle } from 'react-icons/bi';
 
 import etcStyles from '../styles/etc.module.css';
 
 import Navigation from '../components/navbar';
 import Footer from '../components/footer';
+import { useContext } from 'react';
 
-const HomeContent = () => {
+interface Toggle{
+  children: React.ReactNode,
+  eventKey: string,
+  callback?: Function
+}
+
+const AwareToggle = ({children, eventKey, callback}: Toggle) => {
+  const currentEventKey = useContext(AccordionContext);
+  const decoratedOnClick = useAccordionToggle(eventKey, () => callback && callback(eventKey));
+  const isCurrentEventKey: boolean = currentEventKey === eventKey;
+
+  return(
+    <Button
+      variant={isCurrentEventKey ? "info" : "light"}
+      onClick={decoratedOnClick}
+    >
+      {children}
+    </Button>
+  );
+}
+
+interface HomeContent{
+  children: React.ReactNode
+}
+
+const HomeContent = ({children}: HomeContent) => {
   return(
     <Container className=''>
+      {children}
       <Row>
         <Col>
           <Jumbotron className='d-flex flex-column align-items-center text-center'>
@@ -38,6 +65,56 @@ const HomeContent = () => {
           </Carousel>
         </Col>
       </Row>
+
+      <Row className='mt-4'>
+        <Col>
+          <Jumbotron>
+            <h3 className='text-center'>The Ins &amp; Outs of Recycling</h3>
+            <Accordion className='mt-3'>
+              <Card>
+                <Card.Header>
+                  <AwareToggle eventKey='0'>What Is Recycling?</AwareToggle>
+                </Card.Header>
+
+                <Accordion.Collapse eventKey='0'>
+                  <>
+                    <Card.Img src='https://images.unsplash.com/photo-1481761289552-381112059e05?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1161&q=80'/>
+                    <Card.Body>
+                      Simply put, recycling is the process of converting waste materials into new materials! Of course,
+                      there's more to it than just creating new from the old. The process is also beneficial towards the surrounding environment
+                      in many ways!
+                    </Card.Body>
+                  </>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
+          </Jumbotron>
+        </Col>
+
+        <Col>
+          <Jumbotron>
+            <h3 className='text-center'>Ways To Contribute</h3>
+            <Accordion className='mt-3'>
+              <Card>
+                <Card.Header>
+                  <AwareToggle eventKey='0'>Recycling Guidelines</AwareToggle>
+                </Card.Header>
+
+                <Accordion.Collapse eventKey='0'>
+                  <>
+                    <Card.Img src='https://pixfeeds.com/images/save-nature/1280-508877770-different-colored-recycle-waste-bins.jpg'/>
+                    <Card.Body>
+                      One of the key steps in the journey towards contributing is learning about reycling guidelines!
+                      You can head over to the EPA's (Environment Protection Agency) website to learn the basics! You may
+                      also want to search the web for guidelines specific to your community!
+                    </Card.Body>
+                  </>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
+          </Jumbotron>
+        </Col>
+      </Row>
     </Container>
   )
 }
@@ -46,21 +123,21 @@ const HomeContent = () => {
 const Home = () => {
   return(
     <>
-      <Head>
-        <title>RecycleIT</title>
-        <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
-          crossOrigin="anonymous"
-        />
-      </Head>
-
       <Navigation/>
-      <HomeContent/>
+      <HomeContent>
+        <Head>
+          <title>RecycleIT Home</title>
+          <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+            integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+            crossOrigin="anonymous"
+          />
+        </Head>
+      </HomeContent>
       <Footer/>
     </>
-  )
+  );
 }
 
 export default Home;
--- pages/map/index.tsx
+++ pages/map/index.tsx
@@ -1,7 +1,44 @@
-const MapPage = () => {
+import Head from 'next/head';
+import {Container, Row, Col} from 'react-bootstrap';
+
+import Footer from '../../components/footer';
+import Navigation from '../../components/navbar';
+
+interface MapContent{
+    children: React.ReactNode
+}
+
+const MapContent = ({children}: MapContent) => {
     return(
-        <h1>Pain</h1>
+        <Container>
+            {children}
+            <Row>
+                <Col>
+                    <h3>Map Coming Soon!</h3>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
-export default MapPage;
+const Map = () => {
+    return(
+        <>
+            <Navigation/>
+            <MapContent>
+                <Head>
+                    <title>RecycleIT Map</title>
+                    <link
+                        rel="stylesheet"
+                        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+                        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+                        crossOrigin="anonymous"
+                    />
+                </Head>
+            </MapContent>
+            <Footer/>
+        </>
+    )
+}
+
+export default Map
