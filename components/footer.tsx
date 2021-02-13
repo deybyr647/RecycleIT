e58a143ff8f79aeae5653bc1214aa2064e30c2c8
deybyr647 <deybyr647@gmail.com>
2021-02-13 00:43:42 -0500

    Minor readability improvement

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,13 +1,13 @@
-import {Nav} from 'react-bootstrap';
+import {Nav} from "react-bootstrap";
 
 const Footer = () => {
     return(
-        <Nav className='sticky-bottom justify-content-center bg-light mt-3'>
-            <Nav.Link href='https://deybyr647.com' target='_blank' rel='noopener noreferrer'>
+        <Nav className="sticky-bottom justify-content-center bg-light mt-3">
+            <Nav.Link href="https://deybyr647.com" target="_blank" rel="noopener noreferrer">
                 &copy; 2021 | Deyby Rodriguez
             </Nav.Link>
         </Nav>
-    )
-}
+    );
+};
 
 export default Footer;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,31 +1,31 @@
-import Link from 'next/link';
-import {Nav, Navbar} from 'react-bootstrap';
-import {FaRecycle} from 'react-icons/fa';
+import Link from "next/link";
+import {Nav, Navbar} from "react-bootstrap";
+import {FaRecycle} from "react-icons/fa";
 
-import styles from '../styles/etc.module.css';
+import styles from "../styles/etc.module.css";
 
 const Navigation = () => {
     return(
-        <Navbar bg='light' variant='light' expand='lg' className='mb-3'>
-            <Navbar.Brand className={styles.anchors}><FaRecycle className={styles.faRecycle}/> <Link href='/'>RecycleIT</Link></Navbar.Brand>
-            <Navbar.Toggle aria-controls='nav'/>
-            <Navbar.Collapse id='nav'>
+        <Navbar bg="light" variant="light" expand="lg" className="mb-3">
+            <Navbar.Brand className={styles.anchors}><FaRecycle className={styles.faRecycle}/> <Link href="/">RecycleIT</Link></Navbar.Brand>
+            <Navbar.Toggle aria-controls="nav"/>
+            <Navbar.Collapse id="nav">
                 <Nav className={`ml-auto text-center ${styles.anchors}`}>
-                    <Nav.Item className='nav-link'>
-                        <Link href='/map'>Map</Link>
+                    <Nav.Item className="nav-link">
+                        <Link href="/map">Map</Link>
                     </Nav.Item>
 
-                    <Nav.Item className='nav-link'>
-                        <Link href='/news'>News</Link>
+                    <Nav.Item className="nav-link">
+                        <Link href="/news">News</Link>
                     </Nav.Item>
 
-                    <Nav.Item className='nav-link'>
-                        <Link href='/about'>About</Link>
+                    <Nav.Item className="nav-link">
+                        <Link href="/about">About</Link>
                     </Nav.Item>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
-    )
-}
+    );
+};
 
 export default Navigation;
