919858b0fd6bc3bbe5759d8f3bb1c79906bda5a0
deybyr647 <deybyr647@gmail.com>
2021-02-13 00:02:38 -0500

    Abstracted head/metadata component, and made changes based on comments, as requested. Ready for merge?

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,22 @@
-import Head from 'next/head';
-import { Container, Row, Col, Jumbotron, Image, Carousel, Accordion, Card, Button, AccordionContext, useAccordionToggle } from 'react-bootstrap';
-import { BiRecycle } from 'react-icons/bi';
+import { useContext } from "react";
 
-import etcStyles from '../styles/etc.module.css';
+import { Container, Row, Col, Jumbotron, Image, Carousel, Accordion, Card, Button, AccordionContext, useAccordionToggle } from "react-bootstrap";
+import { BiRecycle } from "react-icons/bi";
+import etcStyles from "../styles/etc.module.css";
 
-import Navigation from '../components/navbar';
-import Footer from '../components/footer';
-import { useContext } from 'react';
+import MetaData from "../components/metadata";
+import Navigation from "../components/navbar";
+import Footer from "../components/footer";
 
-interface Toggle{
+interface ContextAwareToggleProps {
   children: React.ReactNode,
   eventKey: string,
-  callback?: Function
-}
+  onToggle?: Function
+};
 
-interface HomeContent{
-  children: React.ReactNode
-}
-
-const AwareToggle = ({children, eventKey, callback}: Toggle) => {
+const ContextAwareToggle = ({children, eventKey, onToggle}: ContextAwareToggleProps) => {
   const currentEventKey = useContext(AccordionContext);
-  const decoratedOnClick = useAccordionToggle(eventKey, () => callback && callback(eventKey));
+  const decoratedOnClick = useAccordionToggle(eventKey, () => onToggle && onToggle(eventKey));
   const isCurrentEventKey: boolean = currentEventKey === eventKey;
 
   return(
@@ -31,57 +27,56 @@ const AwareToggle = ({children, eventKey, callback}: Toggle) => {
       {children}
     </Button>
   );
-}
+};
 
-const HomeContent = ({children}: HomeContent) => {
+const HomeContent = () => {
   return(
-    <Container className=''>
-      {children}
+    <Container>
       <Row>
         <Col>
-          <Jumbotron className='d-flex flex-column align-items-center text-center'>
-            <Image src='https://images.unsplash.com/photo-1604187351574-c75ca79f5807?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80' alt='Recycling Bin' className='w-75 shadow-lg' rounded/>
-            <Container className='d-flex flex-row justify-content-between text-center mt-3'>
+          <Jumbotron className="d-flex flex-column align-items-center text-center">
+            <Image src="https://images.unsplash.com/photo-1604187351574-c75ca79f5807?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="Recycling Bin" className="w-75 shadow-lg" rounded/>
+            <Container className="d-flex flex-row justify-content-between text-center mt-3">
               <BiRecycle size={52} className={etcStyles.faRecycle}/>
-              <h1 className='mx-3'>RecycleIT</h1>
+              <h1 className="mx-3">RecycleIT</h1>
               <BiRecycle size={52} className={etcStyles.faRecycle}/>
             </Container>
 
-            <p className='mt-2'>A new, simple way to recycling</p>
+            <p className="mt-2">A new, simple way to recycling</p>
           </Jumbotron>
         </Col>
       </Row>
 
-      <Row className=''>
+      <Row>
         <Col>
           <Carousel>
             <Carousel.Item>
-              <Image src='https://images.unsplash.com/photo-1604187351574-c75ca79f5807?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80' className='d-block w-100'/>
+              <Image src="https://images.unsplash.com/photo-1604187351574-c75ca79f5807?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" className="d-block w-100"/>
             </Carousel.Item>
 
             <Carousel.Item>
-              <Image src='https://images.unsplash.com/photo-1513615040015-9ab281b0f9e7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80' className='d-block w-100'/>
+              <Image src="https://images.unsplash.com/photo-1513615040015-9ab281b0f9e7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80" className="d-block w-100"/>
             </Carousel.Item>
           </Carousel>
         </Col>
       </Row>
 
-      <Row className='mt-4'>
+      <Row className="mt-4">
         <Col>
           <Jumbotron>
-            <h3 className='text-center'>The Ins &amp; Outs of Recycling</h3>
-            <Accordion className='mt-3'>
+            <h3 className="text-center">The Ins &amp; Outs of Recycling</h3>
+            <Accordion className="mt-3">
               <Card>
                 <Card.Header>
-                  <AwareToggle eventKey='0'>What Is Recycling?</AwareToggle>
+                  <ContextAwareToggle eventKey="0">What Is Recycling?</ContextAwareToggle>
                 </Card.Header>
 
-                <Accordion.Collapse eventKey='0'>
+                <Accordion.Collapse eventKey="0">
                   <>
-                    <Card.Img src='https://images.unsplash.com/photo-1481761289552-381112059e05?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1161&q=80'/>
+                    <Card.Img src="https://images.unsplash.com/photo-1481761289552-381112059e05?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1161&q=80"/>
                     <Card.Body>
                       Simply put, recycling is the process of converting waste materials into new materials! Of course,
-                      there's more to it than just creating new from the old. The process is also beneficial towards the surrounding environment
+                      there"s more to it than just creating new from the old. The process is also beneficial towards the surrounding environment
                       in many ways!
                     </Card.Body>
                   </>
@@ -93,19 +88,19 @@ const HomeContent = ({children}: HomeContent) => {
 
         <Col>
           <Jumbotron>
-            <h3 className='text-center'>Ways To Contribute</h3>
-            <Accordion className='mt-3'>
+            <h3 className="text-center">Ways To Contribute</h3>
+            <Accordion className="mt-3">
               <Card>
                 <Card.Header>
-                  <AwareToggle eventKey='0'>Recycling Guidelines</AwareToggle>
+                  <ContextAwareToggle eventKey="0">Recycling Guidelines</ContextAwareToggle>
                 </Card.Header>
 
-                <Accordion.Collapse eventKey='0'>
+                <Accordion.Collapse eventKey="0">
                   <>
-                    <Card.Img src='https://pixfeeds.com/images/save-nature/1280-508877770-different-colored-recycle-waste-bins.jpg'/>
+                    <Card.Img src="https://pixfeeds.com/images/save-nature/1280-508877770-different-colored-recycle-waste-bins.jpg"/>
                     <Card.Body>
                       One of the key steps in the journey towards contributing is learning about reycling guidelines!
-                      You can head over to the EPA's (Environment Protection Agency) website to learn the basics! You may
+                      You can head over to the EPA"s (Environment Protection Agency) website to learn the basics! You may
                       also want to search the web for guidelines specific to your community!
                     </Card.Body>
                   </>
@@ -116,28 +111,19 @@ const HomeContent = ({children}: HomeContent) => {
         </Col>
       </Row>
     </Container>
-  )
-}
+  );
+};
 
 
 const Home = () => {
   return(
     <>
+      <MetaData title="ReycleIT Home"/>
       <Navigation/>
-      <HomeContent>
-        <Head>
-          <title>RecycleIT Home</title>
-          <link
-            rel="stylesheet"
-            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-            integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
-            crossOrigin="anonymous"
-          />
-        </Head>
-      </HomeContent>
+      <HomeContent/>
       <Footer/>
     </>
   );
-}
+};
 
 export default Home;
--- pages/map/index.tsx
+++ pages/map/index.tsx
@@ -1,41 +1,38 @@
-import Head from 'next/head';
-import { useState } from 'react';
-import {Container, Row, Col, Form, Button} from 'react-bootstrap';
-import {BiCurrentLocation, BiSearchAlt, BiStreetView} from 'react-icons/bi';
-import GoogleMapReact from 'google-map-react';
+import { useState } from "react";
+import GoogleMapReact from "google-map-react";
 
-import Footer from '../../components/footer';
-import Navigation from '../../components/navbar';
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { BiCurrentLocation, BiSearchAlt, BiStreetView } from "react-icons/bi";
 
-const mapsKey: any = process.env.NEXT_PUBLIC_GOOGLEMAPS_API_KEY;
+import MetaData from "../../components/metadata";
+import Navigation from "../../components/navbar";
+import Footer from "../../components/footer";
 
-interface Coords{
+const mapsKey: string | undefined = process.env.NEXT_PUBLIC_GOOGLEMAPS_API_KEY;
+
+interface Coords {
     lat: number,
     lng: number
-}
+};
 
-interface GMap{
+interface GMapProps {
     center: Coords
     children?: React.ReactNode
-}   
+};   
 
-interface MapPageContent{
-    children: React.ReactNode
-}
-
-const GMap = ({center, children}: GMap) => {
-    //Creates Google Map
-    const defaultCoords: Coords = {lat: 40.7128, lng: -74.0060}; //Defaults to NYC coords
+const GMap = ({center, children}: GMapProps) => {
+    const defaultCoords: Coords = {lat: 40.7128, lng: -74.0060};
     const mapOptions = {
         mapTypeControl: true,
         streetViewControl: true
-    }
+    };
 
     return(
-        <div className='shadow' style={{width: "100%", height: "80vh"}}>
+        <div className="shadow" style={{width: "100%", height: "80vh"}}>
             {center.lat && center.lng ? 
                 <GoogleMapReact
                     yesIWantToUseGoogleMapApiInternals
+                    //@ts-ignore
                     bootstrapURLKeys={{key: mapsKey, libraries:[]}}
                     defaultCenter={defaultCoords}
                     center={center}
@@ -48,13 +45,14 @@ const GMap = ({center, children}: GMap) => {
                         lng={center.lng}
                         text="Marker"
                         size={26}
-                        style={{position:'absolute', transform: 'translate(-50%, -50%)'}}
+                        style={{position:"absolute", transform: "translate(-50%, -50%)"}}
                     />
 
                     {children}
                 </GoogleMapReact>
                 : 
                 <GoogleMapReact
+                    //@ts-ignore
                     bootstrapURLKeys={{key: mapsKey, libraries:[]}}
                     defaultCenter={defaultCoords}
                     defaultZoom={11}
@@ -63,23 +61,20 @@ const GMap = ({center, children}: GMap) => {
                 </GoogleMapReact>
             }
         </div>
-    )
+    );
 }
 
-const MapPageContent = ({children}: MapPageContent) => {
-    const [zip, setZip] = useState('');
+const MapPageContent = () => {
+    const [zip, setZip] = useState("");
     const [userCoords, setUserCoords] = useState({lat: null, lng: null});
 
     const changeHandler = (e: any): void => {
-        //Sets zip code state from form value
         e.preventDefault();
-
-        //@ts-ignore
+        console.log(zip);
         setZip(e.target.value);
-    }
+    };
 
     const getUserCoords = (e: any): void => {
-        //Sets user coordinates as state
         e.preventDefault();
 
         if(navigator.geolocation){
@@ -94,27 +89,25 @@ const MapPageContent = ({children}: MapPageContent) => {
                         ...prev,
                         lat: userCoords.lat,
                         lng: userCoords.lng
-                    }
+                    };
                 });
             });
-        }
-    }
+        };
+    };
 
     return(
         <Container>
-            {children}
-
-            <Row className='mb-2'>
+            <Row className="mb-2">
                 <Col>
-                    <Form className='d-flex flex-row justify-content-between' onSubmit={e => e.preventDefault()}>
-                        <Form.Control required type='zip' placeholder='Enter Zip Code...' value={zip} onChange={(e: any) => changeHandler(e)}></Form.Control>
-                        <Button type='submit' className='mx-2'><BiSearchAlt/></Button>
-                        <Button type='submit' onClick={(e: any) => getUserCoords(e)}><BiCurrentLocation/></Button>
+                    <Form className="d-flex flex-row justify-content-between" onSubmit={e => e.preventDefault()}>
+                        <Form.Control required placeholder="Enter Zip Code..." value={zip} onChange={changeHandler}/>
+                        <Button type="submit" className="mx-2"><BiSearchAlt/></Button>
+                        <Button type="submit" onClick={getUserCoords}><BiCurrentLocation/></Button>
                     </Form>
                 </Col>
             </Row>
 
-            <Row className='mt-2'>
+            <Row className="mt-2">
                 <Col>
                     <GMap
                         //@ts-ignore
@@ -123,27 +116,18 @@ const MapPageContent = ({children}: MapPageContent) => {
                 </Col>
             </Row>
         </Container>
-    )
+    );
 }
 
 const MapPage = () => {
     return(
         <>
+            <MetaData title="RecycleIT Map"/>
             <Navigation/>
-            <MapPageContent>
-                <Head>
-                    <title>RecycleIT Map</title>
-                    <link
-                        rel="stylesheet"
-                        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-                        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
-                        crossOrigin="anonymous"
-                    />
-                </Head>
-            </MapPageContent>
+            <MapPageContent/>
             <Footer/>
         </>
-    )
-}
+    );
+};
 
 export default MapPage;
