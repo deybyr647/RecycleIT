b37cc1fb9e63160f2b42ec20db54e93d2b268079
deybyr647 <deybyr647@gmail.com>
2021-03-12 00:15:57 -0500

    Basic feed page design & code

--- pages/feed/index.tsx
+++ pages/feed/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import { Container, Row, Col, Jumbotron, Image, Card, ListGroup } from 'react-bootstrap';
+import { Container, Row, Col, Jumbotron, Image, Card, CardColumns, ListGroup, Carousel } from 'react-bootstrap';
 
 import Metadata from '../../components/Metadata';
 import Navigation from '../../components/Navigation';
@@ -37,11 +37,75 @@ const FeedPageContent = () => {
                 </Col>
             </Row>
 
-            <Row className="d-flex flex-column mt-2">
+            <Row className="mt-2 d-flex flex-column">
                 <Col>
                     <Jumbotron>
-                        <h2>Happening Near You</h2>
+                        <h2 className="text-center">Happening Near You</h2>
                     </Jumbotron>
+                </Col>
+
+                <Col>
+                    <CardColumns>
+                        <Card>
+                            <Card.Img src="https://www.clipartkey.com/mpngs/m/22-220963_science-atom-svg-react-native-logo.png"/>
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste soluta distinctio amet quibusdam voluptatem quis ipsum aliquid deserunt, nobis aperiam aut neque. Quis ratione cupiditate modi, soluta atque ipsa laboriosam.
+                                </Card.Text>
+                            </Card.Body>
+
+                            <Card.Footer>Updated 3 mins ago</Card.Footer>
+                        </Card>
+
+                        <Card>
+                            <Card.Img src="https://www.clipartkey.com/mpngs/m/22-220963_science-atom-svg-react-native-logo.png"/>
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste soluta distinctio amet quibusdam voluptatem quis ipsum aliquid deserunt, nobis aperiam aut neque. Quis ratione cupiditate modi, soluta atque ipsa laboriosam.
+                                </Card.Text>
+                            </Card.Body>
+
+                            <Card.Footer>Updated 3 mins ago</Card.Footer>
+                        </Card>
+
+                        <Card>
+                            <Card.Img src="https://www.clipartkey.com/mpngs/m/22-220963_science-atom-svg-react-native-logo.png"/>
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste soluta distinctio amet quibusdam voluptatem quis ipsum aliquid deserunt, nobis aperiam aut neque. Quis ratione cupiditate modi, soluta atque ipsa laboriosam.
+                                </Card.Text>
+                            </Card.Body>
+
+                            <Card.Footer>Updated 3 mins ago</Card.Footer>
+                        </Card>
+
+                        <Card>
+                            <Card.Img src="https://www.clipartkey.com/mpngs/m/22-220963_science-atom-svg-react-native-logo.png"/>
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste soluta distinctio amet quibusdam voluptatem quis ipsum aliquid deserunt, nobis aperiam aut neque. Quis ratione cupiditate modi, soluta atque ipsa laboriosam.
+                                </Card.Text>
+                            </Card.Body>
+
+                            <Card.Footer>Updated 3 mins ago</Card.Footer>
+                        </Card>
+
+                        <Card>
+                            <Card.Img src="https://www.clipartkey.com/mpngs/m/22-220963_science-atom-svg-react-native-logo.png"/>
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste soluta distinctio amet quibusdam voluptatem quis ipsum aliquid deserunt, nobis aperiam aut neque. Quis ratione cupiditate modi, soluta atque ipsa laboriosam.
+                                </Card.Text>
+                            </Card.Body>
+
+                            <Card.Footer>Updated 3 mins ago</Card.Footer>
+                        </Card>
+                    </CardColumns>
                 </Col>
             </Row>
         </Container>
